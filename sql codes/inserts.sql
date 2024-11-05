04d3e2469f65875fae76dc8f4f8910a67e8ed349
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2024-11-05 15:15:09 -0300

    create inserts

---
+++ sql codes/inserts.sql
@@ -0,0 +1,28 @@
+INSERT INTO pacientes(nome_paciente, email_paciente) VALUES
+('Carmem Gutierrez', 'c.guterrez@gmail.com'),
+('Apolo Cavalcante', 'cavalcanteapolo@gmail.com'),
+('Anthom Front', 'anthom.front@gmail.com'),
+('Eldric', 'el.dricxhy@gmail.com'),
+('Rotív', 'rotivitor@gmail.com'),
+('Jhonny Joe', 'joe.jhonny@gmail.com'),
+('Kaled Silva', 'ka.silva.led@gmail.com'),
+('Kaito Valadares', 'valad.kaito@gmail.com'),
+('Jyro Watanabe', 'jyro.wata@gmail.com'),
+('Ryan Souza', 'souza.ryan@gmail.com');
+
+INSERT INTO pacientes(nome_medico, especialidade_medico) VALUES
+('Brandon Stark', 'ortopedia'),
+('Catelyn Stark', 'otorrinolaringologia'),
+('Sandor Clegane', 'dermatologia'),
+('Aegon Targaryen II', 'dermatologia'),
+('Varys', 'pediatria');
+
+INSERT INTO consultas (data_consulta) VALUES
+('2024-09-27'),
+('2018-10-27'),
+('2020-12-14'),
+('2025-03-02'),
+('2024-11-05'),
+('2023-07-12'),
+('2023-07-10'),
+('2025-04-12');
